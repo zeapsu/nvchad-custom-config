f68062bf239090bee168b78686380c0398c2f630
PaezDev <andryypaez@gmail.com>
2022-11-05 01:39:42 -0700

    fix rust-tools

--- plugins/init.lua
+++ plugins/init.lua
@@ -52,7 +52,16 @@ return {
     ["simrat39/rust-tools.nvim"] = {
         after = "nvim-lspconfig",
         config = function()
-                require('rust-tools').setup({})
+                require('rust-tools').setup({
+                    server = {
+                        on_attach = function (_, bufnr)
+                        -- hover actions
+                        vim.keymap.set("n", "<C-space>", require('rust-tools').hover_actions.hover_actions, { buffer = bufnr })
+                        -- Code action groups
+                        vim.keymap.set("n", "<Leader>a", require('rust-tools').code_action_group.code_action_group, { buffer = bufnr })
+                        end,
+                    }
+                })
         end
     },
 
