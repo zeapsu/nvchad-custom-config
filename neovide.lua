50a1f9385d5442008b2e704fc93f348323e38e70
zeapsu <andryypaez@gmail.com>
2023-10-08 02:56:56 -0700

    fix: neovide

--- neovide.lua
+++ neovide.lua
@@ -7,5 +7,5 @@ g.neovide_transparency = 0.95
 g.neovide_cursor_vfx_mode = "wireframe"
 g.neovide_remember_window_size = true
 g.neovide_hide_mouse_when_typing = true
-g.neovide_fullscreen = false
+g.neovide_fullscreen = true
 g.neovide_refresh_rate = 60
